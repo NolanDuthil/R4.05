82e8502ec0d58f572bf0a890fb4a8bba8afdf174
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-02-08 10:01:57 +0100

    tp3

--- TP3/EX1/exo1.js
+++ TP3/EX1/exo1.js
@@ -12,25 +12,41 @@ const scene = new THREE.Scene();
 
 scene.background = new THREE.CubeTextureLoader().setPath('./assets/').load(['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg']);
 
-let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
 light.position.set(50, 100, 10);
 light.target.position.set(0, 0, 0);
+light.castShadow = true;
 scene.add(light);
-scene.add(new THREE.DirectionalLightHelper(light));
 
-const geometry = new THREE.PlaneGeometry(10, 10);
-const material = new THREE.MeshBasicMaterial({ color: 0xFFC0CB, side: THREE.DoubleSide });
-const plane = new THREE.Mesh(geometry, material);
-plane.rotation.set(Math.PI * 0.5, 0, 0);
-scene.add(plane);
+
+light.shadow.bias = -0.001;
+light.shadow.mapSize.width = 2048;
+light.shadow.mapSize.height = 2048;
+light.shadow.camera.near = 50;
+light.shadow.camera.far = 150;
+light.shadow.camera.left = 100;
+light.shadow.camera.right = -100;
+light.shadow.camera.top = 100;
+light.shadow.camera.bottom = -100;
+
 
 const geometrybox = new THREE.BoxGeometry(2, 5, 1);
 const materialbox = new THREE.MeshBasicMaterial({ color: 0x73C2FB });
 const box = new THREE.Mesh(geometrybox, materialbox);
 box.translateY(4);
+box.castShadow = true; 
+box.receiveShadow = false;
 scene.add(box);
 
-const objects = [];
+const geometry = new THREE.PlaneGeometry(10, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0xFFC0CB, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(geometry, material);
+plane.rotation.set(Math.PI * 0.5, 0, 0);
+plane.receiveShadow = true;
+scene.add(plane);
+
+const helper = new THREE.CameraHelper( light.shadow.camera );
+scene.add( helper );
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
@@ -44,6 +60,8 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas, antialias:true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.render(scene, camera);
 
 window.addEventListener('resize', () => {
@@ -56,6 +74,7 @@ window.addEventListener('resize', () => {
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 const loop = () => {
+
     controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(loop);
